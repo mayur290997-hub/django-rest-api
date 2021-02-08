88532d0e0bdaebb3d4b82100c3b17e827f819ab9
Mayur <mayur.finity@gmail.com>
2021-02-08 13:15:55 +0530

    creating the first api-view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+from profiles_api import serializers
+
+class HelloApiView(APIView):
+    """ test Api View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self,request, format=None):
+        """ Returns the List ApiView Feature"""
+        an_apiview = [
+            'user HTTp method as function(get, post, patch,put,delete)',
+            'Is similar to a traditional django View',
+            'Gives you the most control over you application logic',
+            'Is mappped manually to URls'
+        ]
+
+        return Response({'message':'Hello!' ,'an_apiview':an_apiview})
+
+    def post(self,request):
+        """ create the hello massage with our name """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status= status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        """ Handle Updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """ Handle a partial update of an object """
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """ Delete the request of an object """
+        return Response({'method':'DELETE'})
+
